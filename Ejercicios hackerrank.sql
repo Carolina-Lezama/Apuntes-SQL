c659ba83a1737b5386eafaa5e824505155d5e264
Carolina-Lezama <gesenigan@gmail.com>
2026-06-16 21:21:33 -0600

    16

--- Ejercicios hackerrank.sql
+++ Ejercicios hackerrank.sql
@@ -351,3 +351,23 @@ INNER JOIN Grades gr
     ON st.Marks BETWEEN gr.min_mark AND gr.max_mark
 ORDER BY gr.Grade DESC, Name ASC, st.Marks ASC; 
 
+/*
+Julia just finished conducting a coding contest, and she needs your help assembling the leaderboard! Write a query to print the respective hacker_id and name of hackers who achieved full scores for more than one challenge. 
+Order your output in descending order by the total number of challenges in which the hacker earned a full score. 
+If more than one hacker received full scores in same number of challenges, then sort them by ascending hacker_id.
+*/
+
+SELECT
+    s.hacker_id ,
+    h.name
+FROM Submissions s
+INNER JOIN Challenges c
+    ON s.challenge_id = c.challenge_id
+INNER JOIN Difficulty d
+    ON c.difficulty_level = d.difficulty_level
+INNER JOIN Hackers h
+    ON s.hacker_id = h.hacker_id
+WHERE s.score = d.score
+GROUP BY s.hacker_id, h.name
+HAVING COUNT(*) > 1 
+ORDER BY COUNT(*) DESC, s.hacker_id ASC;
