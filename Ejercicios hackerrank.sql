84a5823a851e06f8a148e3b3a3b74a40288d2eb5
Carolina-Lezama <gesenigan@gmail.com>
2026-06-17 18:26:08 -0600

    16

--- Ejercicios hackerrank.sql
+++ Ejercicios hackerrank.sql
@@ -371,3 +371,32 @@ WHERE s.score = d.score
 GROUP BY s.hacker_id, h.name
 HAVING COUNT(*) > 1 
 ORDER BY COUNT(*) DESC, s.hacker_id ASC;
+
+/*
+Harry Potter and his friends are at Ollivander's with Ron, finally replacing Charlie's old broken wand.
+Hermione decides the best way to choose is by determining the minimum number of gold galleons needed to buy each non-evil wand of high power and age. 
+Write a query to print the id, age, coins_needed, and power of the wands that Ron's interested in, sorted in order of descending power. 
+If more than one wand has same power, sort the result in order of descending age.
+*/
+
+WITH agrupamiento AS (
+    SELECT
+    w.id,
+    ROW_NUMBER() OVER(PARTITION BY  w.power, wp.age ORDER BY w.coins_needed ASC ) AS minimo,
+    wp.age,
+    w.coins_needed,
+    w.power
+FROM Wands w
+INNER JOIN Wands_Property wp
+    ON w.code = wp.code
+WHERE wp.is_evil = 0
+)
+
+SELECT
+    id,
+    age,
+    coins_needed,
+    power
+FROM agrupamiento
+WHERE minimo = 1
+ORDER BY power DESC, age DESC
