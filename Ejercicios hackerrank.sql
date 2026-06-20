2c0d6ddd912eda18c864c639dd00a9ec050eff51
Carolina-Lezama <gesenigan@gmail.com>
2026-06-19 19:30:56 -0600

    16

--- Ejercicios hackerrank.sql
+++ Ejercicios hackerrank.sql
@@ -434,3 +434,25 @@ SELECT hacker_id, name, total_retos
 FROM Analisis_Ventanas
 WHERE total_retos = maximo_historico OR empates = 1
 ORDER BY total_retos DESC, hacker_id ASC;
+
+/*
+You are given a table, BST, containing two columns: N and P, where N represents the value of a node in Binary Tree, and P is the parent of N.
+Write a query to find the node type of Binary Tree ordered by the value of the node. Output one of the following for each node:
+Root: If node is root node.
+Leaf: If node is leaf node.
+Inner: If node is neither root nor leaf node.
+*/
+
+SELECT
+    N,
+    CASE
+        WHEN P IS NULL
+            THEN 'Root'
+        WHEN N NOT IN (SELECT P FROM BST WHERE P IS NOT NULL)
+            THEN 'Leaf'
+        WHEN N IN (SELECT P FROM BST)
+            THEN 'Inner'
+        ELSE 'unkown'
+    END AS type
+FROM BST
+ORDER BY N
