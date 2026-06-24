5e81f53e513f5ca86585348c0baccb67edc05cd4
Carolina-Lezama <gesenigan@gmail.com>
2026-06-23 22:35:43 -0600

    23

--- Ejercicios hackerrank.sql
+++ Ejercicios hackerrank.sql
@@ -456,3 +456,32 @@ SELECT
     END AS type
 FROM BST
 ORDER BY N
+
+/*
+Given the table schemas below, write a query to print the company_code, founder name, total number of lead managers, total number of senior managers, total number of managers, and total number of employees. 
+Order your output by ascending company_code.
+Note:
+The tables may contain duplicate records.
+The company_code is string, so the sorting should not be numeric. 
+For example, if the company_codes are C_1, C_2, and C_10, then the ascending company_codes will be C_1, C_10, and C_2.
+*/
+
+SELECT
+    c.company_code,
+    c.founder, 
+    COUNT(DISTINCT l.lead_manager_code) AS total_lead_m, 
+    COUNT(DISTINCT s.senior_manager_code) AS total_senior_m,
+    COUNT(DISTINCT m.manager_code) AS total_manager,
+    COUNT(DISTINCT e.employee_code ) AS total_employee 
+FROM Company c
+LEFT JOIN Lead_Manager l
+    ON c.company_code = l.company_code
+LEFT JOIN Senior_Manager s
+    ON c.company_code = s.company_code
+LEFT JOIN Manager m
+    ON c.company_code = m.company_code
+LEFT JOIN Employee e
+    ON c.company_code = e.company_code
+GROUP BY c.company_code, c.founder
+ORDER BY c.company_code ASC
+
