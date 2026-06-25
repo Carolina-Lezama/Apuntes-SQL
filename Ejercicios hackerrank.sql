2430386e69b8525d9a65c829202d2e8574977b06
Carolina-Lezama <gesenigan@gmail.com>
2026-06-25 15:11:59 -0600

    25

--- Ejercicios hackerrank.sql
+++ Ejercicios hackerrank.sql
@@ -529,3 +529,33 @@ SELECT
     ROUND(LAT_N, 4)
 FROM  agrupamiento
 WHERE fila = (total + 1) / 2
+
+/*
+You did such a great job helping Julia with her last coding contest challenge that she wants you to work on this one, too!
+The total score of a hacker is the sum of their maximum scores for all of the challenges. 
+Write a query to print the hacker_id, name, and total score of the hackers ordered by the descending score. 
+If more than one hacker achieved the same total score, then sort the result by ascending hacker_id. Exclude all hackers with a total score of  from your result.
+*/
+
+WITH agrupamiento AS (
+    SELECT
+    h.hacker_id,
+    h.name,
+    MAX(s.score) AS maximo
+    FROM Hackers h
+    INNER JOIN Submissions s
+        ON h.hacker_id = s.hacker_id
+    GROUP BY h.hacker_id, s.challenge_id, h.name
+
+)
+
+SELECT
+hacker_id,
+name,
+SUM(maximo) AS total_score
+FROM agrupamiento
+GROUP BY hacker_id, name
+HAVING SUM(maximo) > 0
+ORDER BY total_score DESC, hacker_id ASC
+
+
