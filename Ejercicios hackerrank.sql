fe41a110e6fffe8663f1a62cb9772adc76bc0904
Carolina-Lezama <gesenigan@gmail.com>
2026-06-29 14:41:37 -0600

    29

--- Ejercicios hackerrank.sql
+++ Ejercicios hackerrank.sql
@@ -580,3 +580,39 @@ WHERE X = Y
 GROUP BY X, Y
 HAVING COUNT(*) > 1
 ORDER BY X ASC;
+
+
+/*
+Cuando en SQL pones dos tablas (o subconsultas) en el FROM separadas por una coma, estás haciendo lo que en matemáticas se llama un Producto Cartesiano (también conocido como CROSS JOIN). 
+Básicamente, SQL agarra cada fila de la primera caja y la combina con todas las filas de la segunda caja.
+*/
+
+/*
+You are given a table, Projects, containing three columns: Task_ID, Start_Date and End_Date. 
+It is guaranteed that the difference between the End_Date and the Start_Date is equal to 1 day for each row in the table.
+If the End_Date of the tasks are consecutive, then they are part of the same project. 
+Samantha is interested in finding the total number of different projects completed.
+Write a query to output the start and end dates of projects listed by the number of days it took to complete the project in ascending order. 
+If there is more than one project that have the same number of completion days, then order by the start date of the project.
+*/
+
+SELECT
+Inicios.Start_Date,
+MIN(Finales.End_Date)
+FROM 
+    ( -- Saca los Inicios Reales
+      SELECT Start_Date 
+      FROM Projects 
+      WHERE Start_Date NOT IN (SELECT End_Date FROM Projects)
+    ) AS Inicios,
+    
+    ( -- Saca los Finales Reales
+      SELECT End_Date 
+      FROM Projects 
+      WHERE End_Date NOT IN (SELECT Start_Date FROM Projects)
+    ) AS Finales
+
+WHERE Inicios.Start_Date < Finales.End_Date
+GROUP BY Inicios.Start_Date
+ORDER BY (MIN(Finales.End_Date) - Inicios.Start_Date) ASC, Inicios.Start_Date ASC
+
