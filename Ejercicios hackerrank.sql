ab59de882946f890c7798347b234544a37093cb3
Carolina-Lezama <gesenigan@gmail.com>
2026-06-30 20:45:28 -0600

    30

--- Ejercicios hackerrank.sql
+++ Ejercicios hackerrank.sql
@@ -616,3 +616,23 @@ WHERE Inicios.Start_Date < Finales.End_Date
 GROUP BY Inicios.Start_Date
 ORDER BY (MIN(Finales.End_Date) - Inicios.Start_Date) ASC, Inicios.Start_Date ASC
 
+/*
+You are given three tables: Students, Friends and Packages. 
+Students contains two columns: ID and Name. Friends contains two columns: ID and Friend_ID (ID of the ONLY best friend). 
+Packages contains two columns: ID and Salary (offered salary in $ thousands per month).
+Write a query to output the names of those students whose best friends got offered a higher salary than them. 
+Names must be ordered by the salary amount offered to the best friends. 
+It is guaranteed that no two students got same salary offer.
+*/
+
+SELECT
+Name
+FROM Students s
+INNER JOIN Friends f
+    ON s.ID = f.ID
+INNER JOIN Packages p1
+    ON s.ID = p1.ID
+INNER JOIN Packages p2
+    ON f.Friend_ID = p2.ID
+WHERE p2.Salary > p1.Salary
+ORDER BY p2.Salary
