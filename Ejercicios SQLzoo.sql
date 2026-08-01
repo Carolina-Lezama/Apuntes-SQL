95cce87aaa23da22039e51fe4b25667b2bb7aa1c
Carolina-Lezama <gesenigan@gmail.com>
2026-07-31 21:39:46 -0600

    31

--- Ejercicios SQLzoo.sql
+++ Ejercicios SQLzoo.sql
@@ -41,3 +41,44 @@ WHERE area > 3000000 or population > 250000000
 SELECT name, population, area FROM world
 WHERE (area > 3000000 OR population > 250000000) 
   AND NOT (area > 3000000 AND population > 250000000)
+
+-- 9. Rounding
+SELECT name, ROUND(population/1000000.0,2), ROUND(gdp/1000000000.0,2) FROM world
+WHERE continent = 'South America'
+
+-- 10. Trillion dollar economies
+SELECT name, ROUND(gdp/population,-3) FROM world
+WHERE gdp > 1000000000000
+LIMIT 1000
+
+/*
+ROUND(x, 0): Redondea a las unidades 
+ROUND(x, -1): Redondea a las decenas 
+ROUND(x, -2): Redondea a las centenas 
+ROUND(x, -3): Redondea a los millares 
+*/
+
+-- 12. Matching name and capital
+
+SELECT name, capital
+FROM world
+WHERE LEFT(name,1) = LEFT(capital,1) AND name <> capital
+
+/*
+<> as the NOT EQUALS operator.
+*/
+
+-- 11. Name and capital have the same length
+SELECT name, capital
+  FROM world
+ WHERE LENGTH(name) = LENGTH(capital)
+
+-- 13. All the vowels
+SELECT name
+FROM world
+WHERE name LIKE '%a%' 
+  AND name LIKE '%e%' 
+  AND name LIKE '%i%' 
+  AND name LIKE '%o%' 
+  AND name LIKE '%u%'
+  AND name NOT LIKE '% %';
